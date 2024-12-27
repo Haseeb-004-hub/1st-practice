0320779bbc438f4bc35357ad79634864bee02cc3
Haseeb-004-hub <haseeb.prolab@gmail.com>
2024-12-27 23:57:40 +0500

    Add system_info.sh script

---
+++ system_info.sh
@@ -0,0 +1,6 @@
+Your Name: HaseebAlma
+System Name: DESKTOP-IL368LH
+CPU Information: Intel(R) Core(TM) i7-6700HQ CPU @ 2.60GHz (8 CPUs)
+Memory Information: Total RAM: 7.7Gi
+OS Information: AlmaLinux 9.5 (Teal Serval)
+Storage Information: Total SSD Storage: 
